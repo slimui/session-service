53b220918226205393b7ab9d1f60649410923c6e
Harrison Harnisch <hharnisc@gmail.com>
2017-04-04 14:40:02 -0500

    Refactor for case where no accessToken is passed

--- src/server.js
+++ src/server.js
@@ -7,13 +7,16 @@ const proto = grpc.load('./session.proto');
 const redis = new Redis(process.env.SESSION_STORE_LOCATION);
 
 export const create = (call, cb) => { // eslint-disable-line import/prefer-default-export
-  const accessToken = call.request.accessToken;
-  if (!accessToken) {
-    cb('Missing accessToken in session data');
-    return Promise.resolve();
-  }
   const sessionId = v4();
-  return redis.set(sessionId, call.request.accessToken)
+  return new Promise((resolve, reject) => {
+    const accessToken = call.request.accessToken;
+    if (!accessToken) {
+      reject('Missing accessToken in session data');
+    } else {
+      resolve(accessToken);
+    }
+  })
+    .then(accessToken => redis.set(sessionId, accessToken))
     .then(() => new Promise((resolve, reject) => {
       jwt.sign({ sessionId }, process.env.SIGNING_SECRET, {}, (err, token) => {
         if (err) {
