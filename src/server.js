a1c247aa9b55973a22ce68f027953db65ceeac50
Harrison Harnisch <hharnisc@gmail.com>
2017-04-04 10:35:51 -0500

    Simplify and move everything into the server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-console.log('Hi!'); // eslint-disable-line no-console
+import Redis from 'ioredis';
+import { v4 } from 'uuid';
+
+const redis = new Redis(process.env.SESSION_STORE_LOCATION);
+
+export const create = (call, cb) => // eslint-disable-line import/prefer-default-export
+  redis.set(v4(), call.request.accessToken)
+    .then(() => cb());
---
+++ src/server.test.js
@@ -0,0 +1,38 @@
+import { v4, uniqueId } from 'uuid';
+import Redis from 'ioredis';
+import { create } from './server';
+
+describe('server', () => {
+  it('should init ioredis with expected settings', () => {
+    process.env.SESSION_STORE_LOCATION = 'https://my-setting-store';
+    expect(Redis)
+      .toBeCalledWith(process.env.SESSION_STORE_LOCATION);
+  });
+
+  describe('create', () => {
+    it('should call callback on success', () => {
+      const accessToken = 'accessToken';
+      const cb = jest.fn();
+      const call = {
+        request: { accessToken },
+      };
+      return create(call, cb)
+        .then(() => {
+          expect(cb)
+            .toBeCalled();
+        });
+    });
+
+    it('should set item in redis', () => {
+      const accessToken = 'accessToken';
+      const call = {
+        request: { accessToken },
+      };
+      create(call, () => {});
+      expect(v4)
+        .toBeCalled();
+      expect(Redis.prototype.set)
+        .toBeCalledWith(uniqueId, accessToken);
+    });
+  });
+});
