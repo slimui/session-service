9f58b4e18a0e2971401df3bd1e13da51b2e86d22
Harrison Harnisch <hharnisc@gmail.com>
2017-04-04 09:34:28 -0500

    Set session in redis

--- src/create.js
+++ src/create.js
@@ -1,3 +1,8 @@
-const create = (call, cb) => cb();
+import Redis from 'ioredis';
+import { v4 } from 'uuid';
+
+const create = (call, cb) =>
+  Redis.set(v4(), call.request.accessToken)
+    .then(() => cb());
 
 export default create;
--- src/create.test.js
+++ src/create.test.js
@@ -1,4 +1,14 @@
+import uuid from 'uuid';
+import Redis from 'ioredis';
 import create from './create';
+
+jest.mock('uuid', () => ({
+  v4: jest.fn(() => 'unique_id'),
+}));
+
+jest.mock('ioredis', () => ({
+  set: jest.fn(() => Promise.resolve()),
+}));
 
 describe('create', () => {
   it('should call callback on success', () => {
@@ -7,8 +17,22 @@ describe('create', () => {
     const call = {
       request: { accessToken },
     };
-    create(call, cb);
-    expect(cb)
+    return create(call, cb)
+      .then(() => {
+        expect(cb)
+          .toBeCalled();
+      });
+  });
+
+  it('should set item in redis', () => {
+    const accessToken = 'accessToken';
+    const call = {
+      request: { accessToken },
+    };
+    create(call, () => {});
+    expect(uuid.v4)
       .toBeCalled();
+    expect(Redis.set)
+      .toBeCalledWith('unique_id', accessToken);
   });
 });
