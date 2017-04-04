dacd5e1fbdad3663b59221caa86abf56319a11ca
Harrison Harnisch <hharnisc@gmail.com>
2017-04-04 11:30:07 -0500

    Returns a session JWT

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import Redis from 'ioredis';
 import { v4 } from 'uuid';
+import jwt from 'jsonwebtoken';
 
 const redis = new Redis(process.env.SESSION_STORE_LOCATION);
 
-export const create = (call, cb) => // eslint-disable-line import/prefer-default-export
-  redis.set(v4(), call.request.accessToken)
-    .then(() => cb());
+export const create = (call, cb) => { // eslint-disable-line import/prefer-default-export
+  const sessionId = v4();
+  return redis.set(sessionId, call.request.accessToken)
+    .then(() => new Promise((resolve, reject) => {
+      jwt.sign({ sessionId }, process.env.SIGNING_SECRET, {}, (err, token) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(token);
+        }
+      });
+    }))
+    .then(token => cb(undefined, { token }))
+    .catch(err => cb(err));
+};
--- src/server.test.js
+++ src/server.test.js
@@ -1,8 +1,13 @@
-import { v4, uniqueId } from 'uuid';
 import Redis from 'ioredis';
+import jwt from 'jsonwebtoken';
+import { v4, uniqueId } from 'uuid';
 import { create } from './server';
 
 describe('server', () => {
+  const secret = 's3cr3t';
+  beforeEach(() => {
+    process.env.SIGNING_SECRET = secret;
+  });
   describe('create', () => {
     it('should call callback on success', () => {
       const accessToken = 'accessToken';
@@ -27,6 +32,22 @@ describe('server', () => {
         .toBeCalled();
       expect(Redis.prototype.set)
         .toBeCalledWith(uniqueId, accessToken);
+    });
+
+    it('should return a JWT token with the session jwt', () => {
+      const accessToken = 'accessToken';
+      const cb = jest.fn();
+      const call = {
+        request: { accessToken },
+      };
+      const token = jwt.sign({ sessionId: uniqueId }, secret);
+      return create(call, cb)
+        .then(() => {
+          expect(cb)
+            .toBeCalledWith(undefined, {
+              token,
+            });
+        });
     });
   });
 });
